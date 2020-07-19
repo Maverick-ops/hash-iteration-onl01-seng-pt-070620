357da09a7654bbd297b281b40cb809e1e189ec34
Jesse Murphy <jessemurphy1982@gmail.com>
2020-07-19 18:24:12 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,7 +7,13 @@ suite_e: "Crumpet the Elf"
 }
 
 
- 
+ winner = ""
+passengers.each do |suite, name|
+  if suite == :suite_a && name.start_with?("A")
+    winner = name
+  end
+end
+
 # passengers = {
 # suite_a: "Amanda Presley", 
 # suite_b: "Seymour Hoffman", 
